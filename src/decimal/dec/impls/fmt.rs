595edd1eb5d55d068e5f4d08b124c6b2cc709fb7
Willem Wyndham <willem@ahalabs.dev>
2025-11-19 12:23:15 -0300

    feat: fake display too

--- src/decimal/dec/impls/fmt.rs
+++ src/decimal/dec/impls/fmt.rs
@@ -1,54 +1,57 @@
-// use core::fmt::{self, Debug, Display, Formatter, LowerExp, UpperExp};
+use core::fmt::{self, Debug, Display, Formatter, LowerExp, UpperExp};
 
 use crate::decimal::{dec::format, utils, Decimal};
 
-// impl<const N: usize> Display for Decimal<N> {
-//     #[inline]
-//     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-//         if self.is_nan() {
-//             write!(f, "NaN")
-//         } else if self.is_infinite() {
-//             write!(f, "{}Inf", self.sign())
-//         } else {
-//             format::format(
-//                 self.digits.to_str_radix(10),
-//                 self.cb.get_scale(),
-//                 self.sign(),
-//                 f,
-//             )
-//         }
-//     }
-// }
+impl<const N: usize> Display for Decimal<N> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        todo!()
+        // if self.is_nan() {
+        //     write!(f, "NaN")
+        // } else if self.is_infinite() {
+        //     write!(f, "{}Inf", self.sign())
+        // } else {
+        //     format::format(
+        //         self.digits.to_str_radix(10),
+        //         self.cb.get_scale(),
+        //         self.sign(),
+        //         f,
+        //     )
+        // }
+    }
+}
 
-// impl<const N: usize> LowerExp for Decimal<N> {
-//     #[inline]
-//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-//         format::format_exponential(
-//             self.digits.to_str_radix(10),
-//             self.cb.get_scale(),
-//             self.sign(),
-//             f,
-//             "e",
-//         )
-//     }
-// }
+impl<const N: usize> LowerExp for Decimal<N> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        todo!()
+        // format::format_exponential(
+        //     self.digits.to_str_radix(10),
+        //     self.cb.get_scale(),
+        //     self.sign(),
+        //     f,
+        //     "e",
+        // )
+    }
+}
 
-// impl<const N: usize> UpperExp for Decimal<N> {
-//     #[inline]
-//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-//         format::format_exponential(
-//             self.digits.to_str_radix(10),
-//             self.cb.get_scale(),
-//             self.sign(),
-//             f,
-//             "E",
-//         )
-//     }
-// }
-
-impl<const N: usize> core::fmt::Debug for Decimal<N> {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+impl<const N: usize> UpperExp for Decimal<N> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         todo!();
-    //     utils::fmt::debug_print(&self.digits, &self.cb, Self::type_name(), f)
+        // format::format_exponential(
+        //     self.digits.to_str_radix(10),
+        //     self.cb.get_scale(),
+        //     self.sign(),
+        //     f,
+        //     "E",
+        // )
+    }
+}
+
+impl<const N: usize> Debug for Decimal<N> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        todo!()
+        // utils::fmt::debug_print(&self.digits, &self.cb, Self::type_name(), f)
     }
 }
